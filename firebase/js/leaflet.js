9b73bf6e2601bd095afaf4f32dc5bb8543b5ad53
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2023-08-04 21:40:27 -0300

    atualizacao

--- firebase/js/leaflet.js
+++ firebase/js/leaflet.js
@@ -3,7 +3,7 @@ async function carregaMapa(){
         lat: -23.536692,
         long: -47.443943
     }
-    let map = L.map('map').setView([geo.lat, geo.long], 15)
+    let map = L.map('map').setView([geo.lat, geo.long],15)
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{maxZoom:19, attribution: 'OpenStreetMap'}).addTo(map)
 
     await firebase.database().ref('estabelecimentos').on('value',(snapshot) => {
@@ -16,3 +16,5 @@ async function carregaMapa(){
         })
     })
 }
+
+
