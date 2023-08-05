5069a26b58c4c4981bc37091ce09077e404e3e55
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2023-08-04 21:06:39 -0300

    att

--- firebase/js/leaflet.js
+++ firebase/js/leaflet.js
@@ -5,4 +5,14 @@ async function carregaMapa(){
     }
     let map = L.map('map').setView([geo.lat, geo.long], 15)
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{maxZoom:19, attribution: 'OpenStreetMap'}).addTo(map)
+
+    await firebase.database().ref('estabelecimentos').on('value',(snapshot) => {
+        snapshot.forEach(item =>{
+            const dados = item.val()
+            const marker = L.marker([dados.latitude, dados.longitude]).addTo(map)
+
+            marker.bindTooltip(dados.razao)
+            marker.bindPopup(`<b>${dados.razao}</b><br> Incluído por: ${dados.usuarioInclusao.nome}`)
+        })
+    })
 }
